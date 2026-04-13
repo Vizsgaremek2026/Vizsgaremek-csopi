4bcb69b4cac913d7841eb495628861147fb1ff67
Balazs873 <lepbal873@hengersor.hu>
2026-04-13 12:15:22 +0200

    Delete App.test.jsx

--- App.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders login screen when not authenticated', () => {
-  render(<App />);
-  expect(screen.getByRole('heading', { name: /Log in/i })).toBeInTheDocument();
-});
