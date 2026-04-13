f3ee27a9d21978f26963c7eece34f77acf22ded1
Balazs873 <lepbal873@hengersor.hu>
2026-04-13 12:14:51 +0200

    Delete api directory

--- api/searchHistory.js
+++
@@ -1,9 +0,0 @@
-import { apiRequest } from "./client";
-
-export function saveSearchHistory(userEmail, query) {
-  return apiRequest("/api/search-history", {
-    method: "POST",
-    userEmail,
-    body: JSON.stringify({ query }),
-  });
-}
